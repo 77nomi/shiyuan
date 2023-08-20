27538b52de58fcb552ba4147a79e59b56dea874d
unknown <unknown@example.com>
2023-08-21 02:01:53 +0800

    改了立即帮助那部分的内容

--- pages/shouye/help-page-detail/help-page-detail.js
+++ pages/shouye/help-page-detail/help-page-detail.js
@@ -1,5 +1,10 @@
 Page({
   data: {
+    msg:'',
+    num:'',
+    type1:0,
+    type2:0,
+   flag:'false',
     bonus: '',
     comNum: '',
     contact: '',
@@ -16,12 +21,15 @@ Page({
     status: '',
     title: '',
     userId: '',
+    // 报错 userId is not defined
     userImage: '',
     userName: '',
   },
   onLoad(options) {
+    // console.log(wx.getStorageSync('id'))
     this.setData({id:options.id})
     this.getDetailData(options.id)
+    this.buttonStatus()
   },
 
   //获取详细信息
@@ -29,7 +37,7 @@ Page({
     var that = this
     wx.showLoading({title: '加载中...',})
     wx.request({
-      url: 'http://8.130.118.211:5795//common/request/' + id,
+      url: 'http://8.130.118.211:5795/common/request/' + id,
       headers: {
         authentication : wx.getStorageSync('token')
       },
@@ -79,4 +87,56 @@ Page({
       urls: imageList
     })
   },
+  
+buttonStatus:function(){
+  console.log(10)
+  if(this.data.userId===wx.getStorageSync('id')){
+      flag:false;
+}else{
+  if(this.data.status===null){
+    flag:true;
+  }
+  else if(this.data.status===1){
+    type1=1;
+    this.setData({
+      msg:'已完成'
+    })
+       }
+  else if(this.data.status===2){
+        type1=1;
+     this.setData({
+           msg:'已被确认'
+           })
+  }
+  else if(this.data.status===3){
+    type1=1;
+    this.setData({
+      msg:'已取消'
+    })
+ }
+ else if(this.data.status===5){
+  type1=1;
+  this.setData({ 
+    msg:'已被取消'
+  })
+}
+ else if(this.data.status===0){
+type1=1;
+this.setData({
+  msg1:'取消帮助',
+  msg2:'完成帮助'
 })
+}
+  else if(this.data.status===4){
+type2=2;
+this.setData({
+  msg1:'取消帮助',
+  msg2:'重新上传'
+})
+}
+  }
+}
+
+  
+})
+
